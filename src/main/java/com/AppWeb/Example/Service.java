064bd32783f30be0d1d434250554826adcbff3de
David Mercado Tut <juan.mercado02@uceva.edu.co>
2026-01-27 21:07:36 -0500

    basic CRUD created and make for a simple working

--- src/main/java/com/AppWeb/Example/Service.java
+++ src/main/java/com/AppWeb/Example/Service.java
@@ -1,4 +1,40 @@
 package com.AppWeb.Example;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.AppWeb.Example.UsersModel;
 public class Service {
+    private List<UsersModel> users = new ArrayList<>();
+
+    public void CreateUsers(UsersModel user) {
+        users.add(user);
+    }
+
+    public List<UsersModel> Read() {
+        return users;
+    }
+
+    public List<UsersModel> Update(int id, String name, String LastName, String Mail) {
+        for (UsersModel u : users) {
+            if (u.getId() == id) {
+                u.setNames(name);
+                u.setLastNames(LastName);
+                u.setMail(Mail);
+                return users;
+            }
+        }
+        return users;
+    }
+
+    public boolean Delete(int id) {
+        for (UsersModel u : users) {
+            if (u.getId() == id) {
+                users.remove(id);
+                return true;
+            }
+
+        }
+        System.out.println("The Id doesnt match in our database");
+        return false;
+    }
 }
